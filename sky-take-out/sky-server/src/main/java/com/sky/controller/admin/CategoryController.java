1c914833c223b8a48d339f0fb9f65f75b845727c
JohnnyTaiCK <126487338+JohnnyTaiCK@users.noreply.github.com>
2025-09-15 01:40:21 +0800

    Adding new Category function

--- sky-take-out/sky-server/src/main/java/com/sky/controller/admin/CategoryController.java
+++ sky-take-out/sky-server/src/main/java/com/sky/controller/admin/CategoryController.java
@@ -2,8 +2,11 @@ package com.sky.controller.admin;
 
 import com.sky.dto.CategoryDTO;
 import com.sky.result.Result;
+import com.sky.service.CategoryService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,8 +18,14 @@ import org.springframework.web.bind.annotation.RestController;
 @Api(tags = "分类相关接口")
 public class CategoryController {
 
+    @Autowired
+    private CategoryService categoryService;
+
     @PostMapping()
+    @ApiOperation("新增分类")
     public Result<String> save(@RequestBody CategoryDTO categoryDTO) {
-        return null;
+        log.info("新增分类：{}", categoryDTO);
+        categoryService.save(categoryDTO);
+        return Result.success();
     }
 }
